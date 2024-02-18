895ddc223096fc23c89c80f9508fa37f7498fac6
Avinash Payak <avinashpayak@gmail.com>
2024-02-19 04:47:36 +0530

    contact section added

---
+++ src/components/sections/Contact.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <div class="min-w[1024px] max-w-[1360px] lg:mx-auto flex flex-col lg:flex-row items-center lg:justify-center lg:gap-10 py-4">
+            <h2 class="lg:text-[100px] text-[steelblue] text-[50px]">Get in Touch</h2>
+            <pre class="whitespace-pre-line text-[lightsteelblue] text-[16px] lg:text-[24px]">Prefer email? Sounds good. 
+                I'm over at <span class="text-red-500 cursor-pointer">avipayak@gmail.com</span></pre>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
